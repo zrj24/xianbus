a5bb5eedf2105f658a93a023d758671bf227fb80
zrj2 <2420398231@qq.com>
2024-11-30 12:47:45 +0800

    add single

--- filelist2md.py
+++ filelist2md.py
@@ -5,6 +5,7 @@ from natsort import os_sorted
 def filelist2md():
     datapath = r'E:\xianbus\241129\diagram'
     filelist = os.listdir(datapath)
+    filelist = [file for file in filelist if file != 'index.md']
     filelist = os_sorted(filelist)
     filenames_without_ext = [os.path.splitext(filename)[0] for filename in filelist]
     # print(filenames_without_ext)
